a05c30c342dfa795d77a19926333784c0fa91985
YosepEChristo <yosepechristo@gmail.com>
2022-05-31 02:16:26 +0700

    Add database & body-parser in main.js

--- server/main.js
+++ server/main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const url = 'mongodb+srv://Admin:IAI123@tihefdb.bra2g.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
 app.use(express.json());
 
-const PORT = 8080;
+
+const PORT = 8000;
 
 
 const userRoutes = require('./Routes/customers');
