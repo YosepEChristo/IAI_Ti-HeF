fc54649c668825c4bd05014f21b9a8061c9d04b0
YosepEChristo <yosepechristo@gmail.com>
2022-05-31 23:33:21 +0700

    install axios

--- server/main.js
+++ server/main.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const url = 'mongodb+srv://Admin:IAI123@tihefdb.bra2g.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
