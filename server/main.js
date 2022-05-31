3518938a51c301ba44c9ac7195d1dd4d3c025517
YosepEChristo <yosepechristo@gmail.com>
2022-05-31 17:57:43 +0700

    add cors

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const url = 'mongodb+srv://Admin:IAI123@tihefdb.bra2g.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
